6ba13c806760c1c1d96ec944add36dea6f71744e
Alam sher <bloomwaxiri@gmail.com>
2025-03-24 08:27:14 +0000

    Datatype

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,40 @@
+"use strict" //treat All js code as newer Version
+
+// alert(3+3) //We are using Node js not browser
+
+console.log(3+
+    3)  //Code readability Should be high
+    
+    
+    console.log("Alam")
+
+    let age=18
+    let isLoggedin=false
+
+
+//number => 2 to the power 53 .... Range find in memory 
+//bigint
+//string => "" Double qoutes would be better
+//boolean => true/false
+//null ... Data type b hai and standalone value hai ..... It is a representation of empty value
+//undefined => jab hamny value defined nhi ki 
+//symbol => to find uniqueness ... In figma use unique too much
+
+
+//Above Are premitive dataTypes
+
+
+//object
+
+console.log(typeof "Alam")
+console.log(typeof undefined)
+console.log(typeof null)
+
+
+
+
+
+
+
+
+
