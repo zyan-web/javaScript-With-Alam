c4f4e9a6a0a957b2cf60fb7b94205f0c7fd7c7c5
Alam sher <bloomwaxiri@gmail.com>
2025-04-15 09:05:16 +0000

    conversion of data types

---
+++ 01_basics/03_datatype_conversion.js
@@ -0,0 +1,23 @@
+let score="abc33"
+// let score2=null
+// let score3=unidentified
+
+console.log(score)
+console.log(typeof score);
+
+
+let valueInnumber=Number(score);
+
+console.log(typeof valueInnumber);
+
+
+let BooleanisLoggedIn=Boolean(isLoggedIn);
+console.log(BooleanisLoggedIn);
+console.log(typeof BooleanisLoggedIn);
+console.log(BooleanisLoggedIn);
+
+let someNumber=33
+console.log
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
