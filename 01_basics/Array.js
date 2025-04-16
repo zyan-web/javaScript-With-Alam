766b205838ef3fdd548e3594242263e5fe82217f
Alam sher <bloomwaxiri@gmail.com>
2025-04-16 11:28:32 +0000

    Array

---
+++ 01_basics/Array.js
@@ -0,0 +1,15 @@
+const car = [
+    "Mercedes" , 
+    "Volvo", 
+    "BMW"];
+
+
+    const cars = [];
+    cars[0]= "Mercedes";
+    cars[1]= "Volvo";
+    cars[2]= "BMW";
+
+
+    //To Access Array reffering to a specific Number 
+    
+    let specific_Car = cars[0];
