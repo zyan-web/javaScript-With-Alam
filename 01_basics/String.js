a160b24b695a0b0c280627977f565d4262d20a4e
Alam sher <bloomwaxiri@gmail.com>
2025-04-23 09:11:45 +0000

    String complere

---
+++ 01_basics/String.js
@@ -0,0 +1,17 @@
+
+let alam = "Hi i am alam waziri";
+
+console.log(alam[5]);
+
+console.log(alam.length);
+console.log(alam.toUpperCase());
+console.log(alam.charAt(6));
+console.log(alam.indexOf('w'));
+console.log(alam.substring(-8 , 4));// gives value between 0 and 4 indexs
+console.log(alam.slice(-8,8)); //The differnce is we can give naegtive value too and it will start from reverse
+
+const newstringone = " Alam sher   "
+console.log(newstringone.trim())
+
+
+
