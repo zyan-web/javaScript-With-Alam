99b2b609b59c4ab9bad4d065b76a046dde061f4c
Alam sher <bloomwaxiri@gmail.com>
2025-05-01 08:52:44 +0000

    object part 3

---
+++ 02_basics/Object_part3.js
@@ -0,0 +1,27 @@
+//De structuring of Object
+
+const course =  {
+    coursename: "Js in hindi",
+    price: "999",
+    coursestructor: "Hitesh"
+    }
+    
+    //course.coursestructor //To print once
+    
+    //suppsoe we want to print it mulitple times
+    //Curely braces { mean} de structuring
+    //we can de structure array too
+    const {coursestructor: instructor} = course  //Now the name is change to instructor.. This is called de structure
+    console.log(instructor);
+    
+    //Jason >>> 
+    // {
+    
+    // "name":"Alam",
+    // "coursenamee":"js with Alam",
+    // "price": "free"
+    
+    // }
+    
+    
+    
