58aba6d5ae1db6f60a430237bc26e414a86fff8c
Alam sher <bloomwaxiri@gmail.com>
2025-05-03 10:29:51 +0000

    One

---
+++ 05_Iterations/one.js
@@ -0,0 +1,22 @@
+//for Loop
+//CTRL + D >>>>> for duplicate
+for (let i = 0; i <= 20; i++) {
+    const element = i;
+if(element ==  5){
+//console.log("5 is best number");
+}
+
+   //console.log(element); 
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop value: ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        
+        //console.log(`Inner loop value: ${j} and inner loop ${i}`);
+        console.log(i + '*' + j + '=' + i*j);
+        
+    }
+    
+}
