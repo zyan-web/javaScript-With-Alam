099145afec4530d70a06ab728a71285f10b1cef8
Alam sher <bloomwaxiri@gmail.com>
2025-05-05 06:25:04 +0000

    While and do while loop

--- 05_Iterations/one.js
+++ 05_Iterations/one.js
@@ -3,20 +3,55 @@
 for (let i = 0; i <= 20; i++) {
     const element = i;
 if(element ==  5){
-//console.log("5 is best number");
+// console.log("5 is best number");
 }
 
-   //console.log(element); 
+//    console.log(element); 
 }
 
 for (let i = 0; i <= 10; i++) {
-    console.log(`Outer loop value: ${i}`);
+    // console.log(`Outer loop value: ${i}`);
     
-    for (let j = 0; j <= 10; j++) {
+    for (let j = 1; j <= 10; j++) {
         
-        //console.log(`Inner loop value: ${j} and inner loop ${i}`);
-        console.log(i + '*' + j + '=' + i*j);
-        
+        // console.log(`Inner loop value: ${j} and inner loop ${i}`); //inner loop 10 bar chalega
+        // console.log(i + '*' + j + '=' + i*j);
+        // 
     }
     
 }
+
+
+let myArray = ["Flah" , "Batman" , "Superman"]
+// console.log(myArray.length);
+
+
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+
+
+//====================== Keywords =========================
+
+//break and continue
+
+for (let index = 1; index <= 20; index++) {
+    // const element = array[index];
+
+if(index ==  5){
+
+    console.log(`You have reached to ${index}`);
+    // break;
+    continue;
+    
+}
+
+console.log(`value of i is ${index}`);
+
+// console.log(`value of i is ${index}`);
+
+    
+}
+
---
+++ 05_Iterations/whileDoWhile.js
@@ -0,0 +1,30 @@
+//While and Do While loop
+let index = 0
+while (index <=  10) {
+    // console.log(`value of index is ${index}`);
+  
+
+    //We must have to keep any statement to stop the lop from infinite 
+    index =index + 1
+}
+
+let myArray = ["flash" , "Batman" , "Superman"]
+let arr = 0
+
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`)
+
+    arr = arr + 1   
+}
+
+//================== Do while =================
+
+let score = 11
+do {
+    //Pahle kaam hoga phr condition
+
+console.log(`socre is ${score}`);
+score++
+
+    
+} while (score <=10);
